e516cf1dba1ca04b34cb6bfd32f49c0cde3ed8c6
Daniel Solis <daniel.solis5200@gmail.com>
2023-04-16 15:57:15 -0600

    vite config ruta dinamica

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/porfolio_yellow/",
+  base: import.meta.env?.BASE_URL ? '/porfolio_yellow/' : '/',
 })
